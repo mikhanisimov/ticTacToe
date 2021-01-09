19764957daac6f7287a60c86250c152c405d0955
rostislav <rostislav.anisimov@gmail.com>
2021-01-09 21:17:30 +0200

    Add Register and CellClick

---
+++ PWA2/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PWA2.Models
+{
+    public class Player
+    {
+        public string TableName { get; set; }
+        public string Name { get; set; }
+        public string ConnectionId { get; set; }
+        public DateTime Registered { get; set; }
+    }
+}
--- PWA2/Services/HubService.cs
+++ PWA2/Services/HubService.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using PWA2.Models;
 
 namespace PWA2.Services
 {
     public class HubService:Hub
     {
+        List<Player> Players = new List<Player>();
+
         public async Task Send(string message)
         {
             await this.Clients.All.SendAsync("Send", message);
         }
+
+        public void Register(string tableName, string playerName)
+        {
+            Player player = new Player();
+            player.Name = playerName;
+            player.ConnectionId = Context.ConnectionId;
+            player.TableName = tableName;
+            player.Registered = DateTime.Now;
+            Players.Add(player);
+        }
+
+        public async Task CellClick(string playerName, string tableName, int i, int j)
+        {
+            string connectionId = "";
+            if(Players.Count(x=>x.TableName == tableName) == 2)
+            {
+                connectionId = Players.First(x => x.Name != playerName).ConnectionId;
+                await this.Clients.Client(connectionId).SendAsync("CellClick", i, j);
+            }            
+        }
+        
     }
 }
